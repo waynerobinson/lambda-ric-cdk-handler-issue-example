7830bf40eb2548da871aa66c3a10de2552173b9f
Wayne Robinson <wayne.robinson@gmail.com>
2025-11-24 01:24:47 +0000

    Initial commit

---
+++ test/lambda_ric_cdk_issue.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib/core';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as LambdaRicCdkIssue from '../lib/lambda_ric_cdk_issue-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/lambda_ric_cdk_issue-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new LambdaRicCdkIssue.LambdaRicCdkIssueStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
